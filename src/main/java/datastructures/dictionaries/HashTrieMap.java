138850df89c78e9ff22e3e87077bde2c74714699
Ethan Le <ethanle4@cs.washington.edu>
2024-02-18 00:04:24 -0800

    funni

--- src/main/java/datastructures/dictionaries/HashTrieMap.java
+++ src/main/java/datastructures/dictionaries/HashTrieMap.java
@@ -23,7 +23,7 @@ public class HashTrieMap<A extends Comparable<A>, K extends BString<A>, V> exten
         }
 
         public HashTrieNode(V value) {
-            this.pointers = (Map<A, HashTrieNode>) new ChainingHashTable<>(()->new MoveToFrontList<>());
+            this.pointers = new HashMap<A, HashTrieNode>();
             this.value = value;
         }
 
@@ -114,7 +114,35 @@ public class HashTrieMap<A extends Comparable<A>, K extends BString<A>, V> exten
 
     @Override
     public void delete(K key) {
-        throw new UnsupportedOperationException();
+        if(key==null){
+            throw new IllegalArgumentException();
+        }
+        ArrayStack<HashTrieNode>psps=new ArrayStack<HashTrieNode>();
+        ArrayStack<A> charch=new ArrayStack<A>();
+        HashTrieNode ayy=(HashTrieNode)this.root;
+        for(A curr:key){
+            if(!ayy.pointers.containsKey(curr)){
+                return;
+            }
+            psps.add(ayy);
+            charch.add(curr);
+            ayy=ayy.pointers.get(curr);
+        }
+        if(ayy.value!=null) {
+            this.size--;
+            ayy.value = null;
+            while (psps.size() > 0) {
+                A pop=charch.next();
+                HashTrieNode rem = psps.next();
+                if (rem.pointers.containsKey(pop) && ayy.pointers.size()==0) {
+                    rem.pointers.remove(pop);
+                }
+                if(rem.value!=null||rem.pointers.size()>0){
+                    return;
+                }
+            }
+        }
+
     }
 
     @Override
--- src/main/java/p2/sorts/TopKSort.java
+++ src/main/java/p2/sorts/TopKSort.java
@@ -1,6 +1,7 @@
 package p2.sorts;
 
 import cse332.exceptions.NotYetImplementedException;
+import datastructures.worklists.MinFourHeap;
 
 import java.util.Comparator;
 
@@ -11,6 +12,21 @@ public class TopKSort {
 
 
     public static <E> void sort(E[] array, int k, Comparator<E> comparator) {
-        throw new NotYetImplementedException();
+        MinFourHeap<E> heap= new MinFourHeap<E>(comparator);
+        if(k>array.length){
+            k=array.length;
+        }
+        for(int i=0;i<k;i++){
+            heap.add(array[i]);
+        }
+        for(int i=k;i<array.length;i++){
+            if(comparator.compare(heap.peek(),array[i])<0){
+                heap.next();
+                heap.add(array[i]);
+            }
+        }
+        for(int i=0;i<k;i++){
+            array[i]=heap.next();
+        }
     }
 }
